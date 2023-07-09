f02dfa35983dbfd204035416f612cf3fa3133b47
Alexey Khristolubov <alex-khrist98@mail.ru>
2023-07-09 12:16:49 +0500

    Adds create and read operations on users table

--- app/db_logic.py
+++ app/db_logic.py
@@ -16,26 +16,46 @@ async def create_connection():
 
 
 async def fetch_user(user: models.User):
-    with await create_connection() as conn:
-        cursor = conn.cursor()
-        conn.row_factory = sqlite3.Row
-        cursor.execute("SELECT * FROM users WHERE emila=?", (user.email, ))
-        user = cursor.fetchall()
-        return user
+    try:
+        with await create_connection() as conn:
+            cursor = conn.cursor()
+            conn.row_factory = sqlite3.Row
+            cursor.execute("SELECT * FROM users WHERE email=?", (user.email, ))
+            user = cursor.fetchall()
+            return user
+    except:
+        logger.exception("An exception has occured")
 
 async def add_user(user: models.User):
-    with await create_connection() as conn:
-        cursor = conn.cursor()
-        conn.row_factory = sqlite3.Row
-        cursor.execute("BEGIN")
-        cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?)", (user.first_name,
-                                                                 user.last_name,
-                                                                 user.email,
-                                                                 user.password, ))
-        conn.commit()
-        logger.info("New user have been created")
+    try:
+        with await create_connection() as conn:
+            cursor = conn.cursor()
+            conn.row_factory = sqlite3.Row
+            cursor.execute("BEGIN")
+            cursor.execute("INSERT INTO users (first_name, last_name, email, password)"
+                           " VALUES (?, ?, ?, ?)", (user.first_name,
+                                                                     user.last_name,
+                                                                     user.email,
+                                                                     user.password, ))
+            conn.commit()
+            logger.info("New user have been created")
+    except:
+        logger.exception("An exception has occured")
 
 async def set_up_db():
-    with create_connection() as conn:
+    with await create_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT,')")
+        query = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT," \
+                "first_name TEXT," \
+                "last_name TEXT," \
+                "email TEXT," \
+                "password TEXT)"
+        cursor.execute("BEGIN")
+        cursor.execute(query)
+        cursor.execute("COMMIT")
+        conn.commit()
+        logger.info("Database is set up successfully")
+
+if __name__ != "__main__":
+    asyncio.create_task(set_up_db())
+
